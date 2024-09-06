acd223419a84d05f8b4b3025f83345a8427dfaf7
NegarOstad <s224283@student.dtu.dk>
2024-09-06 10:34:45 +0200

    Remove unused 'logo' import to fix linting issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <h2>This the my first depolment</h2>
     </div>
   );
 }
